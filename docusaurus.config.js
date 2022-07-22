df221311d9d238eaa35e29cd536ec2a13b8b52c1
snake <admin@viasnake.com>
2022-07-23 00:16:33 +0900

    copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -116,7 +116,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `© ${new Date().getFullYear()} MCPlayNetwork. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
