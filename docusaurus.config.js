a23d8f96f870cc9029c515613aaffe05f072f07e
snake <admin@viasnake.com>
2022-08-27 15:50:05 +0900

    Darkmode as default

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -122,6 +122,11 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
     }),
 };
 
