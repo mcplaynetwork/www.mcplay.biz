4aec3571cdbf6470d06b15c0c132059db6e4466d
snake <admin@viasnake.com>
2023-08-26 17:07:06 +0900

    style: フォームの背景をライトモードで固定する

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,8 +4,8 @@ import {useColorMode} from '@docusaurus/theme-common';
 export default function Form() {
   const {colorMode} = useColorMode();
   return (
-    <div>
-      <script async id="7fa49584-20f8-4084-8c52-fea7b660ce17" data-yt-url="https://support.alflag.org" src="https://support.alflag.org/static/simplified/form/form-entry.js" data-theme={colorMode === 'dark' ? 'dark' : 'light'} data-lang="ja"></script>
+    <div style={{backgroundColor: '#fff', borderRadius: '4px'}}>
+      <script async id="7fa49584-20f8-4084-8c52-fea7b660ce17" data-yt-url="https://support.alflag.org" src="https://support.alflag.org/static/simplified/form/form-entry.js" data-theme="light" data-lang="ja"></script>
     </div>
   );
 }
