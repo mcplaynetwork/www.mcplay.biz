24f98821ee406ac73994a18d6a62861a5e443159
snake <admin@viasnake.com>
2023-08-26 17:13:09 +0900

    style: フォームをセンタリングする

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Form() {
   return (
-    <div style={{backgroundColor: '#fff', borderRadius: '4px'}}>
+    <div style={{backgroundColor: '#fff', borderRadius: '4px', justifyContent: 'center'}}>
       <script async id="7fa49584-20f8-4084-8c52-fea7b660ce17" data-yt-url="https://support.alflag.org" src="https://support.alflag.org/static/simplified/form/form-entry.js" data-theme="light" data-lang="ja"></script>
     </div>
   );
