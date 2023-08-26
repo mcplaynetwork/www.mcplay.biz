19f5a3f549298fc1e65600269fa201f02ac4707b
snake <admin@viasnake.com>
2023-08-26 17:12:33 +0900

    refactor: 不要コードの削除

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import {useColorMode} from '@docusaurus/theme-common';
 
 export default function Form() {
-  const {colorMode} = useColorMode();
   return (
     <div style={{backgroundColor: '#fff', borderRadius: '4px'}}>
       <script async id="7fa49584-20f8-4084-8c52-fea7b660ce17" data-yt-url="https://support.alflag.org" src="https://support.alflag.org/static/simplified/form/form-entry.js" data-theme="light" data-lang="ja"></script>
